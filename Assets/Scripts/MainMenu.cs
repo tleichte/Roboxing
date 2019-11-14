fcbffff6126311e60939ad4e47145d3ffa96a7ec
tleichte <tleichte@mtu.edu>
2019-11-14 12:50:02 -0500

    Started Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public MainMenuOption[] Options;
+
+    private int _option;
+    private int CurrOption {
+        get { return _option; }
+        set {
+            foreach (var opt in Options) opt.DeHighlight();
+            _option = Mathf.Clamp(value, 0, Options.Length);
+            Options[_option].Highlight();
+        }
+    }
+    private MainMenuOption CurrOptionValue => Options[_option]; 
+
+    private int selectedOption;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrOption = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(HGDCabKeys.P1.Top1)) {
+            Confirm();
+        }
+        if (Input.GetKeyDown(HGDCabKeys.P1.JoyUp)) {
+            CurrOption--;
+        }
+        if (Input.GetKeyDown(HGDCabKeys.P1.JoyDown)) {
+            CurrOption++;
+        }
+    }
+
+    private void Confirm() {
+        switch (CurrOptionValue.Type) {
+            case MainMenuOptionType.Play:
+                SceneManager.LoadScene("PreGame");
+                break;
+            case MainMenuOptionType.Quit:
+                Application.Quit();
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuOption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MainMenuOptionType { Play, Quit }
+
+public class MainMenuOption : MonoBehaviour
+{
+
+    public MainMenuOptionType Type;
+
+    public GameObject LeftSelect;
+    public GameObject RightSelect;
+
+    public void Highlight() {
+        LeftSelect.SetActive(true);
+        RightSelect.SetActive(true);
+    }
+    public void DeHighlight() {
+        LeftSelect.SetActive(false);
+        RightSelect.SetActive(false);
+    }
+}
