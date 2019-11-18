df537ef651d7ee5bea67c0a048a76744e37dbf97
Tyler Eichten <tleichte@mtu.edu>
2019-11-18 01:38:10 -0500

    Restructured Scripts

--- Assets/Scripts/Player/PlayerAssetSetter.cs
+++ Assets/Scripts/Player/PlayerAssetSetter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppManager : MonoBehaviour
+public class PlayerAssetSetter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
