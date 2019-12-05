6d28525158ca0ac2c424b21ffdb2709c5210833f
tleichte <tleichte@mtu.edu>
2019-12-05 16:19:34 -0500

    Added boot scene and adjusted curtain UI

---
+++ Assets/Scripts/Boot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boot : MonoBehaviour
+{
+    public CanvasGroup CanvasGroup;
+    
+
+    public float fadeTime;
+    public AnimationCurve fadeCurve;
+
+    private float time;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        CanvasGroup.alpha = 0;
+        time = 0;
+        while (time < fadeTime) {
+            yield return null;
+            time += Time.deltaTime;
+
+            float t = time / fadeTime;
+            CanvasGroup.alpha = fadeCurve.Evaluate(t);
+        }
+
+        CanvasGroup.alpha = 1;
+
+        yield return new WaitForSeconds(3.5f);
+
+        CurtainTransition.Inst.Close(() => {
+            SceneManager.LoadScene("MainMenu");
+        });
+    }
+
+    
+}
