1cf0167bcf764f4f4069ac73019c4275b95e5a54
Tyler Eichten <tleichte@mtu.edu>
2019-12-09 20:35:06 -0500

    Started adding environment

---
+++ Assets/Scripts/Environment/Spectator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spectator : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.Play($"Cheer1", -1, Random.Range(0, 1f));
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
