652150f328ab47d9dbefebae3e9d6ee05566d773
tleichte <tleichte@mtu.edu>
2020-01-02 14:07:55 -0600

    Adjusted boot animation

--- Assets/Scripts/Boot.cs
+++ Assets/Scripts/Boot.cs
@@ -5,45 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class Boot : MonoBehaviour
 {
-    public CanvasGroup CanvasGroup;
+    //public CanvasGroup CanvasGroup;
     
 
-    public float fadeTime;
-    public AnimationCurve fadeCurve;
+    //public float fadeTime;
+    //public AnimationCurve fadeCurve;
 
-    public float VisibleTime;
+    //public float VisibleTime;
 
-    private float time;
+    //private float time;
 
 
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
         Screen.fullScreen = true;
         Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
         QualitySettings.vSyncCount = 1;
 
+        //CanvasGroup.alpha = 0;
+        //time = 0;
+        //while (time < fadeTime) {
+        //    yield return null;
+        //    time += Time.deltaTime;
+
+        //    float t = time / fadeTime;
+        //    CanvasGroup.alpha = fadeCurve.Evaluate(t);
+        //}
+
+        //CanvasGroup.alpha = 1; 
         AudioManager.Inst.PlayOneShot("BootMusic");
+    }
 
-        CanvasGroup.alpha = 0;
-        time = 0;
-        while (time < fadeTime) {
-            yield return null;
-            time += Time.deltaTime;
-
-            float t = time / fadeTime;
-            CanvasGroup.alpha = fadeCurve.Evaluate(t);
-        }
-
-        CanvasGroup.alpha = 1;
-
-        yield return new WaitForSeconds(VisibleTime);
-
+    public void A_GoToMainMenu() {
         CurtainTransition.Inst.Close(() => {
-            AudioManager.Inst.MMSToMenu();
             SceneManager.LoadScene("MainMenu");
         });
     }
-
     
 }
