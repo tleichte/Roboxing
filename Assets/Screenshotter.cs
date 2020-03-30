4bf9f252678f22ee020d56c1fd694308e15a2b53
Tyler Eichten <tleichte@mtu.edu>
2020-03-29 19:35:00 -0500

    Added screenshotter

---
+++ Assets/Screenshotter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshotter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C) && Input.GetKeyDown(KeyCode.Return)) {
+            ScreenCapture.CaptureScreenshot("Screenshot.png");
+            Debug.Log("Screenshotted!");
+        }
+    }
+}
